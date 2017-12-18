946a18e99625aacfc4be0fa9c7540a577c4c0196
unknown <kizilov.yura@gmail.com name = Volizik>
2017-12-19 01:14:15 +0300

    responsive

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,3 +4,18 @@ import $ from 'jquery';
 $(() => {
 	svg4everybody();
 });
+
+$(document).on('click', '.menu-btn', function () {
+	$(this).toggleClass('menu-btn__active');
+	if ($(this).hasClass('menu-btn__active')) {
+        $('.col:first-of-type').css({
+            width: '100%'
+        });
+	}
+	else
+	{
+        $('.col:first-of-type').css({
+            width: '0'
+        });
+	}
+});
---
+++ dist/assets/scripts/0.3d288c63574b88110926.hot-update.js
@@ -0,0 +1,29 @@
+webpackHotUpdate(0,[
+/* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+	
+	var _svg4everybody = __webpack_require__(1);
+	
+	var _svg4everybody2 = _interopRequireDefault(_svg4everybody);
+	
+	var _jquery = __webpack_require__(2);
+	
+	var _jquery2 = _interopRequireDefault(_jquery);
+	
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+	
+	(0, _jquery2.default)(function () {
+		(0, _svg4everybody2.default)();
+	});
+	
+	(0, _jquery2.default)(document).on('click', '.menu-btn', function () {
+		(0, _jquery2.default)('.col:first-of-type').css({
+			width: '300px'
+		});
+	});
+
+/***/ }
+])
+//# sourceMappingURL=0.3d288c63574b88110926.hot-update.js.map
